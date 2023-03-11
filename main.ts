417f047eb5336d46490b63179b3a17c6696d768c
L7Cy <l7cy.at@gmail.com>
2023-03-11 12:49:30 +0900

    リファクタリング

--- main.ts
+++ main.ts
@@ -1,5 +1,54 @@
 import { Plugin, WorkspaceLeaf, addIcon, ItemView } from "obsidian";
 
+export default class TaskSchedulePlugin extends Plugin {
+    scheduleView: ScheduleView | null = null;
+
+    async onload() {
+        console.log("TaskSchedulePlugin: onload");
+
+        this.addCommand({
+            id: "toggle-schedule",
+            name: "Toggle Task Schedule",
+            callback: async () => {
+                const activeFile = this.app.workspace.getActiveFile();
+                if (!activeFile) return;
+                const content = await this.app.vault.read(activeFile);
+                const tasks = content.split("\n").filter((line) => line.startsWith("- [ ]"));
+
+                const leaves = this.app.workspace.getLeavesOfType("task-schedule");
+
+                if (leaves.length > 0) {
+                    // 既に開かれているビューがある場合、閉じる
+                    this.app.workspace.detachLeavesOfType("task-schedule");
+                } else {
+                    // 新しくビューを開く
+                    const leaf = this.app.workspace.getRightLeaf(false);
+                    await leaf.setViewState({ type: "task-schedule" });
+                    this.app.workspace.revealLeaf(leaf);
+
+                    // Viewの登録
+                    if (!this.scheduleView) {
+                        this.scheduleView = new ScheduleView(leaf, tasks);
+                        this.registerView("task-schedule", () => this.scheduleView!);
+                        this.registerEvent(
+                            this.app.vault.on("modify", (file) => {
+                                if (file === activeFile && this.scheduleView) {
+                                    this.scheduleView.update();
+                                }
+                            })
+                        );
+                    }
+
+                }
+            },
+        });
+    }
+
+    onunload() {
+        console.log("TaskSchedulePlugin: onunload");
+    }
+}
+
 // Viewの型を定義
 interface ScheduleView extends ItemView {
     containerEl: HTMLDivElement;
@@ -57,56 +106,5 @@ class ScheduleView extends ItemView {
         scheduleTable += "</table>";
         const tableDiv = contentEl.createDiv();
         tableDiv.innerHTML = scheduleTable;
-    }
-}
-
-export default class TaskSchedulePlugin extends Plugin {
-    scheduleView: ScheduleView | null = null;
-
-    async onload() {
-        console.log("TaskSchedulePlugin: onload");
-
-        this.addCommand({
-            id: "toggle-schedule",
-            name: "Toggle Task Schedule",
-            callback: async () => {
-                const activeFile = this.app.workspace.getActiveFile();
-                if (activeFile) {
-                    const content = await this.app.vault.read(activeFile);
-                    const tasks = content.split("\n").filter((line) => line.startsWith("- [ ]"));
-
-                    const leaves = this.app.workspace.getLeavesOfType("task-schedule");
-
-                    if (leaves.length > 0) {
-                        // 既に開かれているビューがある場合、閉じる
-                        this.app.workspace.detachLeavesOfType("task-schedule");
-                    } else {
-                        // 新しくビューを開く
-                        const leaf = this.app.workspace.getRightLeaf(false);
-                        await leaf.setViewState({ type: "task-schedule" });
-                        this.app.workspace.revealLeaf(leaf);
-
-                        // Viewの登録
-                        if (!this.scheduleView) {
-                            this.scheduleView = new ScheduleView(leaf, tasks);
-                            this.registerView("task-schedule", () => this.scheduleView!);
-                            this.registerEvent(
-                                this.app.vault.on("modify", (file) => {
-                                    if (file === activeFile && this.scheduleView) {
-                                        this.scheduleView.update();
-                                    }
-                                })
-                            );
-                        }
-
-                    }
-
-                }
-            },
-        });
-    }
-
-    onunload() {
-        console.log("TaskSchedulePlugin: onunload");
     }
 }
