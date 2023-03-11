f1b9887d2a0f48749188eabd03fe29d421063dbe
L7Cy <l7cy.at@gmail.com>
2023-03-11 09:52:18 +0900

    ビューで表示するように変更

--- main.ts
+++ main.ts
@@ -1,58 +1,112 @@
-import { App, Modal, Plugin, TFile } from "obsidian";
+import { Plugin, WorkspaceLeaf, addIcon, ItemView } from "obsidian";
 
-class TaskScheduleModal extends Modal {
-	tasks: string[];
+// Viewの型を定義
+interface ScheduleView extends ItemView {
+    containerEl: HTMLDivElement;
+    update(): Promise<void>;
+}
 
-	constructor(app: App, tasks: string[]) {
-		super(app);
-		this.tasks = tasks;
-	}
+// ビューを定義
+class ScheduleView extends ItemView {
+    tasks: string[];
 
-	onOpen() {
-		let { contentEl } = this;
+    constructor(leaf: WorkspaceLeaf, tasks: string[]) {
+        super(leaf);
+        this.tasks = tasks;
+    }
 
-		let currentTime = new Date();
-		let scheduleTable = "<table><tr><th>tasks</th><th>estimate</th><th>end</th></tr>";
+    getViewType(): string {
+        return "task-schedule";
+    }
 
-		for (let task of this.tasks) {
-			let [taskName, timeEstimate] = task.split(":");
-			taskName = taskName.replace(/^-\s\[.\]\s/, '').replace(/\[\[|\]\]/g, '').trim();
-			let minutes = parseInt(timeEstimate);
-			let endTime = new Date(currentTime.getTime() + minutes * 60000);
-			let endTimeStr = endTime.getHours().toString().padStart(2, "0") + ":" + endTime.getMinutes().toString().padStart(2, "0");
-			scheduleTable += `<tr><td>${taskName}</td><td>${timeEstimate}m</td><td>${endTimeStr}</td></tr>`;
-			currentTime = endTime;
-		}
+    getDisplayText(): string {
+        return "Task Schedule";
+    }
 
-		scheduleTable += "</table>";
-		contentEl.innerHTML = scheduleTable;
-	}
+    async onOpen(): Promise<void> {
+        await this.update();
+    }
 
-	onClose() {
-		let { contentEl } = this;
-		contentEl.empty();
-	}
+    async onClose(): Promise<void> {
+        // 特に何もしない
+    }
+
+    async update(): Promise<void> {
+        const { contentEl } = this;
+        contentEl.innerHTML = '';
+
+        const activeFile = this.app.workspace.getActiveFile();
+        if (!activeFile) return;
+
+        const content = await this.app.vault.read(activeFile);
+        const tasks = content.split("\n").filter((line) => line.startsWith("- [ ]"));
+
+        let currentTime = new Date();
+        let scheduleTable = "<table><tr><th>tasks</th><th>estimate</th><th>end</th></tr>";
+
+        for (let task of tasks) {
+            let [taskName, timeEstimate] = task.split(":");
+            taskName = taskName.replace(/^-\s\[.\]\s/, '').replace(/\[\[|\]\]/g, '').trim();
+            let minutes = parseInt(timeEstimate);
+            let endTime = new Date(currentTime.getTime() + minutes * 60000);
+            let endTimeStr = endTime.getHours().toString().padStart(2, "0") + ":" + endTime.getMinutes().toString().padStart(2, "0");
+            scheduleTable += `<tr><td>${taskName}</td><td>${timeEstimate}m</td><td>${endTimeStr}</td></tr>`;
+            currentTime = endTime;
+        }
+
+        scheduleTable += "</table>";
+        const tableDiv = contentEl.createDiv();
+        tableDiv.innerHTML = scheduleTable;
+    }
 }
 
 export default class TaskSchedulePlugin extends Plugin {
-	async onload() {
-		console.log("TaskSchedulePlugin: onload");
+    scheduleView: ScheduleView | null = null;
 
-		this.addCommand({
-			id: "show-schedule",
-			name: "show-schedule",
-			callback: async () => {
-				const activeFile = this.app.workspace.getActiveFile();
-				if (activeFile) {
-					const content = await this.app.vault.read(activeFile);
-					const tasks = content.split("\n").filter((line) => line.startsWith("- [ ]"));
-					new TaskScheduleModal(this.app, tasks).open();
-				}
-			},
-		});
-	}
+    async onload() {
+        console.log("TaskSchedulePlugin: onload");
 
-	onunload() {
-		console.log("TaskSchedulePlugin: onunload");
-	}
+        this.addCommand({
+            id: "show-schedule",
+            name: "show-schedule",
+            callback: async () => {
+                const activeFile = this.app.workspace.getActiveFile();
+                if (activeFile) {
+                    const content = await this.app.vault.read(activeFile);
+                    const tasks = content.split("\n").filter((line) => line.startsWith("- [ ]"));
+
+                    // ビューを開く
+                    const leaf = this.app.workspace.getRightLeaf(false);
+                    if (leaf) {
+                        if (leaf.view instanceof ScheduleView) {
+                            await leaf.setViewState({ type: "empty" });
+                            return;
+                        }
+                        await leaf.setViewState({ type: "task-schedule" });
+                        this.app.workspace.revealLeaf(leaf);
+
+                        // Viewの登録
+                        if (!this.scheduleView) {
+                            this.scheduleView = new ScheduleView(leaf, tasks);
+                            this.registerView("task-schedule", () => this.scheduleView!);
+                        } else {
+                            this.scheduleView.tasks = tasks;
+                            await this.scheduleView.update();
+                        }
+
+                        this.registerEvent(
+                            this.app.vault.on("modify", (file) => {
+                                if (file === activeFile && this.scheduleView) {
+                                    this.scheduleView.update();
+                                }
+                            })
+                        );
+                    }
+                }
+            },
+        });
+    }
+    onunload() {
+        console.log("TaskSchedulePlugin: onunload");
+    }
 }
