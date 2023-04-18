203256c458a98c452fe66ba997809c02702ea886
L7Cy <l7cy.at@gmail.com>
2023-04-18 10:01:19 +0900

    開始予定時刻の設定で日付を指定可能にした

--- main.ts
+++ main.ts
@@ -250,7 +250,7 @@ class TaskParser {
         const taskNameRegex = /^-\s*\[\s*.\s*\]\s*/;
         const linkRegex = /\[\[([^\[\]]*\|)?([^\[\]]+)\]\]/g;
         const markdownLinkRegex = /\[([^\[\]]+)\]\(.+?\)/g;
-        const estimateAndStartTimeRegex = new RegExp(`(\\${this.separator}\\s*\\d+\\s*|\\${this.startTimeDelimiter}\\s*\\d{1,2}:\\d{2})`, 'g');
+        const estimateAndStartTimeRegex = new RegExp(`(\\${this.separator}\\s*\\d+\\s*|\\${this.startTimeDelimiter}\\s*(?:\\d{4}-\\d{2}-\\d{2}T)?\\d{1,2}:\\d{2})`, 'g');
 
         return taskName
             .replace(taskNameRegex, "")
@@ -262,14 +262,26 @@ class TaskParser {
     }
 
     public parseStartTime(task: string): Date | null {
-        const regex = new RegExp(`\\${this.startTimeDelimiter}\\s*(\\d{1,2}:\\d{2})`);
-        const match = task.match(regex);
-        if (match) {
+        const timeRegex = new RegExp(`\\${this.startTimeDelimiter}\\s*(\\d{1,2}:\\d{2})`);
+        const dateTimeRegex = new RegExp(`\\${this.startTimeDelimiter}\\s*(\\d{4}-\\d{2}-\\d{2}T\\d{1,2}:\\d{2})`);
+
+        const timeMatch = task.match(timeRegex);
+        const dateTimeMatch = task.match(dateTimeRegex);
+
+        if (dateTimeMatch) {
+            // Parse "yyyy-MM-ddThh:mm" format
+            const parsedDateTime = new Date(dateTimeMatch[1]);
+            if (!isNaN(parsedDateTime.getTime())) {
+                return parsedDateTime;
+            }
+        } else if (timeMatch) {
+            // Parse "hh:mm" format
             const currentTime = new Date();
-            const [hours, minutes] = match[1].split(":").map(Number);
+            const [hours, minutes] = timeMatch[1].split(":").map(Number);
             const startDate = new Date(currentTime.setHours(hours, minutes));
             return startDate;
         }
+
         return null;
     }
 
